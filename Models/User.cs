53fd7711d7edf81db83636be0b43064e8b2b5cd3
vallal0011@gmail.com <vallal0011@gmail.com>
2023-10-24 15:07:05 +0530

    first_commit

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace asp_net.Models
+
+
+{
+	public class User
+	{ 
+					public int ID { get; set; }
+
+				    public string Name { get; set; }
+
+				    public string PhoneNo { get; set; }
+
+				    public string IsActive { get; set; }
+
+		    
+		
+	}
+}
+
